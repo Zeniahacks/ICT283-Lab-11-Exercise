6a22f20b0d3b3a03da5ef32198aa9c1c7002891d
Zeniahacks <elsondoesminecraft@gmail.com>
2025-11-18 00:31:33 +0800

    Added Statistics namespace and statistical helper functions

--- WeatherData.h
+++ WeatherData.h
@@ -30,61 +30,42 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const WeatherRecord& wr);
 };
 
-/// @brief Custom Map wrapper
-template<typename K, typename V>
-class Map
-{
-private:
-    std::map<K, V> internalMap;
-
-public:
-    Map() = default;
-
-    void insert(const K& key, const V& value)
-    {
-        internalMap[key] = value;
-    }
-
-    bool contains(const K& key) const
-    {
-        return internalMap.find(key) != internalMap.end();
-    }
-
-    V& at(const K& key)
-    {
-        return internalMap.at(key);
-    }
-
-    const V& at(const K& key) const
-    {
-        return internalMap.at(key);
-    }
-};
-
-
-
 /// @class WeatherDataCollection
 class WeatherDataCollection {
 private:
     Bst<WeatherRecord> weatherDataBST;
-    std::map<int, std::vector<WeatherRecord*>> dataByMonth;
+    std::map<int, std::vector<WeatherRecord*>> dataByMonth; // Using custom map
 
 public:
     WeatherDataCollection();
     ~WeatherDataCollection();
 
+    // Data management
     void addWeatherRecord(const WeatherRecord& record);
     void loadFromFiles(const std::string& dataSourceFile);
 
+    // Query operations
     std::vector<WeatherRecord> getDataForMonth(int month) const;
+    std::vector<WeatherRecord> getDataForYearMonth(int year, int month) const;
+
+    // Statistical operations
     double calculateSPCC(int month, const std::string& correlationType) const;
 
+    // Menu option 4 calculations
+    void generateMonthlyStats(int year, const std::string& filename) const;
+
+    // Utility methods
     void displayAllData() const;
     int getTotalRecords() const;
 
 private:
     void parseAndAddRecord(const std::string& line);
     Date parseDate(const std::string& dateTimeString);
+
+    // Statistical helper functions
+    static double calculateMean(const std::vector<double>& values);
+    static double calculateStdDev(const std::vector<double>& values);
+    static double calculateMAD(const std::vector<double>& values);
 };
 
 // Custom Map wrapper for bonus marks
@@ -131,6 +112,15 @@ public:
     typename std::map<K, V>::const_iterator begin() const { return internalMap.begin(); }
     typename std::map<K, V>::const_iterator end() const { return internalMap.end(); }
 };
+
+// Statistical Functions
+namespace Statistics
+{
+    double calculateMean(const std::vector<double>& values);
+    double calculateStdDev(const std::vector<double>& values);
+    double calculateMAD(const std::vector<double>& values);
+    double calculateSPCC(const std::vector<double>& x, const std::vector<double>& y);
+}
 
 // Function declarations for WeatherData.cpp
 void printWeatherRecord(const WeatherRecord& record);
