eb02471e96ae184e6cf9a775e755bd1bf8d3d266
Zeniahacks <elsondoesminecraft@gmail.com>
2025-11-18 15:10:35 +0800

    Added loadData(), displayData() and calculateCorrelations() to main.cpp

--- main.cpp
+++ main.cpp
@@ -40,6 +40,59 @@ private:
         cout << "6. Exit" << endl;
         cout << "==========================================" << endl;
     }
+
+    // Switch choices following the above display menu as provided
+    void processChoice(int choice)
+    {
+
+    }
+
+
+    void loadData()
+    {
+        string filename;
+        cout << "Enter the data source file name: ";
+        cin >> filename;
+
+        weatherData.loadFromFiles(filename);
+        dataLoaded = true;
+        cout << "File name found. Data loading completed." << endl;
+    }
+
+    void displayData()
+    {
+        if (!dataLoaded)
+        {
+            cout << "Please load the data first (Option 1)." << endl;
+            return;
+        }
+
+        weatherData.displayAllData();
+    }
+
+    void calculateCorrelations()
+    {
+        if (!dataLoaded)
+        {
+            cout << "Please load the data first (Option 1)." << endl;
+            return;
+        }
+
+        int month;
+        cout << "Enter month (1-12): ";
+        cin >> month;
+
+        if (month < 1 || month > 12)
+        {
+            cout << "You have entered an invalid month. Please try again." << endl;
+            return;
+        }
+
+        cout << "\nSample Pearson Correlation Coefficient for Month" << endl;
+        cout << "S_T" << weatherData.calculateSPCC(month, "S_T") << endl;
+        cout << "S_R" << weatherData.calculateSPCC(month, "S_R") << endl;
+        cout << "T_R" << weatherData.calculateSPCC(month, "T_R") << endl;
+    }
 };
 
 
@@ -47,6 +100,11 @@ private:
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "ICT283 Lab 11 Exercise" << endl;
+    cout << "======================" << endl;
+
+    Assignment2App app;
+    app.run();
+
     return 0;
 }
